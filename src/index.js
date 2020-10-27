2ae67a8db9b41735fe0051e9d697234685176afb
John Webster <johnrwebs@gmail.com>
2020-10-26 19:21:33 -0600

    added voting, subscription for votes, post>createLink for clarity

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const Mutation = require('./resolvers/Mutation');
 const User = require('./resolvers/User');
 const Link = require('./resolvers/Link');
 const Subscription = require('./resolvers/Subscription');
+const Vote = require('./resolvers/Vote');
 
 const pubsub = new PubSub();
 const prisma = new PrismaClient();
@@ -15,7 +16,8 @@ const resolvers = {
   Mutation,
   User,
   Link,
-  Subscription
+  Subscription,
+  Vote
 };
 
 const server = new GraphQLServer({
